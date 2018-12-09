9262ec2c6f70ed406bced8200149168e4425707f
aleha-druga <aleha.druga@gmail.com>
2018-12-09 16:56:46 +0000

    mido: build custom packages

--- device.mk
+++ device.mk
@@ -50,3 +50,10 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
+# lite packages
+PRODUCT_PACKAGES += \
+    GCalendar \
+    GMessaging \
+    GooglePhotos \
+    PocoLauncher
